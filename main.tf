7ea857f7c003601896d6dc4b1764b6dfb5c2804f
krisadaporn <krisadaporn.p@gmail.com>
2023-12-22 15:39:04 +0000

    update2

--- main.tf
+++ main.tf
@@ -199,7 +199,3 @@ resource "aws_key_pair" "hashicat" {
   public_key = tls_private_key.hashicat.public_key_openssh
 }
 
-module "s3-bucket" {
-  source  = "app.terraform.io/krisadaporn-org-aws/s3-bucket/aws"
-  version = "2.8.0"
-}
